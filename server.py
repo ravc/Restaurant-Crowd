23ede4b403a8b88e27fb48890439a89aff579651
Ruben Vazquez-Chapa <ravc@protonmail.com>
2018-02-03 23:20:47 -0600

    added location, auto requests, and notifications if nothing found

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import random, json
 import sys
 from gmaps_calls import *
 
+latlon = []
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,10 +13,24 @@ def index():
     return render_template('index.html')
 
 @app.route('/find', methods = ['POST','GET'])
-def worker():
+def find():
     data = request.get_data()
+    
+    return look_for(latlon, data)
 
-    return look_for([30.6131, -96.3218], data)
+@app.route('/init', methods = ['POST', 'GET'])
+def init():
+    data = str(request.get_data())
+    
+    x = data.split('&')
+    
+    latitude = float(x[-3].split('=')[1])
+    longitude = float(x[-2].split('=')[1])
+    
+    global latlon
+    latlon = [latitude,longitude]
+    
+    return look_for([latitude,longitude])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
